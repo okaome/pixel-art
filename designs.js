01a266a8344212439de04bac83f405feee9b241e
Assurance <34079823+Assurz@users.noreply.github.com>
2018-06-30 01:59:26 +0100

    updated

--- designs.js
+++ designs.js
@@ -1,11 +1,37 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+const heightInput = $('#input_height');
+const widthInput = $('#input_width');
+const colorInput = $('#colorPicker');
 
+// When size is submitted by the user, call makeGrid()
 function makeGrid() {
     
     // Your code goes here!
-    
+    let heightValue = heightInput.val();
+    let widthValue = widthInput.val();
+    let pixelCanvas = $('#pixel_canvas');
+    pixelCanvas.children().remove(); // clears previously built table if any
+  
+  // grid is built
+    for (let h=0; h < heightValue; h++) {
+      pixelCanvas.append("<tr></tr>");
     }
+    for (let w=0; w < widthValue; w++) {
+      $('tr').append("<td></td>");
+    }
+  } 
+  //Background color of the cell changes, with respective chosen color
+  $('table').on('click', 'td', function() {
+    $(this).css('backgroundColor', colorInput.val() );
+  });
+  
+  // When the size is submitted by the user, we call makeGrid()
+  form = $('#sizePicker');
+  form.submit(function(event) {
+      event.preventDefault(); // This stops the page from refreshing upon submit
+      makeGrid();
+  });
+  
     
