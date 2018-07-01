9c1cede855544c6e2a1ad3c62b4bd5d26bd42d11
teenoh <valentino@grit.systems>
2018-07-02 00:00:24 +0100

    upload files

--- designs.js
+++ designs.js
@@ -1,37 +1,44 @@
-// Select color input
-// Select size input
 
-const heightInput = $("#inputHeight");
-const widthInput = $("#inputWidth");
-const colorInput = $("#colorPicker");
+
+// Select  inputHeight(number of rows)
+var heightInput = $("#inputHeight");
+// Select  inputHeight(number of columns)
+var widthInput = $("#inputWidth");
+
+// Select color input
+var colorInput = $("#colorPicker");
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-  let heightValue = heightInput.val();
-  let widthValue = widthInput.val();
-  let pixelCanvas = $("#pixelCanvas");
+  var heightValue = heightInput.val();
+  var widthValue = widthInput.val();
+  var pixelCanvas = $("#pixelCanvas");
 
-  pixelCanvas.children().remove(); // clears previously built table if any
+  // clears previously built table if any exists
+  pixelCanvas.children().remove(); 
 
   // grid is built
-  for (let h = 0; h < heightValue; h++) {
+  for (var row = 0; row < heightValue; row++) {
+    //add a new row
     pixelCanvas.append("<tr></tr>");
 
-    for (let w = 0; w < widthValue; w++) {
-      $(pixelCanvas.children()[h]).append("<td></td>");
+    //add all the columns in the row
+    for (var column = 0; column < widthValue; column++) {
+      $(pixelCanvas.children()[row]).append("<td></td>");
     }
   }
 }
-//Background color of the cell changes, with respective chosen color
 
+//Background color of the cell changes, with respective chosen color
 $("table").on("click", "td", function() {
   $(this).css("backgroundColor", colorInput.val());
 });
 
 // When the size is submitted by the user, we call makeGrid()
-form = $("#sizePicker");
+var form = $("#sizePicker");
 
 form.submit(function(event) {
-  event.preventDefault(); // This stops the page from refreshing upon submit
+  // This stops the page from refreshing upon submit
+  event.preventDefault(); 
   makeGrid();
 });
